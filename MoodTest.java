5fd3a1a5365a08b4b5a9be80ef10e84ddfa8f8da
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-12-08 22:27:57 +0530

    TC2.1 Null mood returns Happy

---
+++ MoodTest.java
@@ -0,0 +1,26 @@
+package com.moodanalyzer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MoodTest {
+
+	@Test
+	public void moodSad() {
+		MoodAnalyzer call = new MoodAnalyzer("I am in sad Mood ");
+		assertEquals("SAD", call.analyseMood());
+	}
+	
+	@Test
+	public void moodHappy() {
+		MoodAnalyzer call = new MoodAnalyzer("I am in happy mood");
+		assertEquals("HAPPY", call.analyseMood());
+	}
+	
+	@Test
+	public void moodNull() {
+		MoodAnalyzer call = new MoodAnalyzer();
+		assertEquals("HAPPY", call.analyseMood());
+	}
+}
