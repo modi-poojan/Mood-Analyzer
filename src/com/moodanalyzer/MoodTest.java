80af0d12992e66161a2552589bf66c4d73e5a65e
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-12-09 00:16:07 +0530

    TestCase_3.1 done sucessfully

--- src/com/moodanalyzer/MoodTest.java
+++ src/com/moodanalyzer/MoodTest.java
@@ -7,15 +7,20 @@ import org.junit.Test;
 public class MoodTest {
 
 	@Test
-	public void moodSad() throws Exception {
+	public void moodSad() throws MoodAnalysisException {
 		MoodAnalyzer call = new MoodAnalyzer("I am in sad Mood ");
 		assertEquals("SAD", call.analyseMood());
 	}
 
 	@Test
-	public void moodHappy() throws Exception {
+	public void moodHappy() throws MoodAnalysisException {
 		MoodAnalyzer call = new MoodAnalyzer("I am in happy mood");
 		assertEquals("HAPPY", call.analyseMood());
 	}
 
+	@Test(expected = MoodAnalysisException.class)
+	public void moodNull() throws MoodAnalysisException {
+		MoodAnalyzer call = new MoodAnalyzer();
+		call.analyseMood();
+	}
 }
